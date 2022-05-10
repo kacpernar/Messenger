5d442cdbe7ec93347fd9157e61b50026d7f64fb9
Numpad <morek1@op.pl>
2022-05-10 20:23:00 +0200

    Js base64

--- Messenger.Blazor/wwwroot/setImageUsingStreaming.js
+++ Messenger.Blazor/wwwroot/setImageUsingStreaming.js
@@ -1,6 +1,13 @@
 ﻿async function setImageUsingStreaming(imageElementId, imageStream) {
+    var base64String;
     const arrayBuffer = await imageStream.arrayBuffer();
     const blob = new Blob([arrayBuffer]);
-    const url = URL.createObjectURL(blob);
-    document.getElementById(imageElementId).src = url;
+
+    var reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onloadend = function () {
+        base64String = reader.result;
+        document.getElementById(imageElementId).src = base64String;
+    }
+    return await base64String;
 }
