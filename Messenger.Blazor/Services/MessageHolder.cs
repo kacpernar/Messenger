6237652f3a69bf25af39526d7bc587aa3d577af5
Kacper <kacpernarloch1@gmail.com>
2022-06-09 17:02:09 +0200

    Fix

--- Messenger.Blazor/Services/MessageHolder.cs
+++ Messenger.Blazor/Services/MessageHolder.cs
@@ -5,41 +5,50 @@ namespace Messenger.Blazor.Services;
 
 public class MessageHolder : IMessageHolder
 {
-    public MessageHolder(NavigationManager navigationManager, EventService eventService)
+    private readonly IUserService _userService;
+
+    public MessageHolder(NavigationManager navigationManager, EventService eventService, IUserService userService)
     {
+        _userService = userService;
         var hubConnection = new HubConnectionBuilder()
             .WithUrl(navigationManager.ToAbsoluteUri("/chathub"))
             .Build();
 
         hubConnection.On<Message>("ReceiveMessage", async message =>
         {
-            switch (message.MessageStatus)
+            if (message.UserName != userService.UserName)
             {
-                case MessageStatus.DeletedToEveryone:
+                switch (message.MessageStatus)
                 {
-                    var messageToDelete = MessageList.Find(m => m.Id == message.Id);
-                    if (messageToDelete != null)
+                    case MessageStatus.DeletedToEveryone:
                     {
-                        await DeleteMessage(messageToDelete);
-                    }
+                        var messageToDelete = MessageList.Find(m => m.Id == message.Id);
+                        if (messageToDelete != null)
+                        {
+                            await DeleteMessage(messageToDelete);
+                        }
 
-                    break;
+                        break;
+                    }
+                    case MessageStatus.DeletedByUser:
+                        return;
+                    case MessageStatus.None:
+                        MessageList.Add(message);
+                        break;
+                    case MessageStatus.Pending:
+                        MessageList.Add(message);
+                        break;
+                    case MessageStatus.StopPending:
+                        MessageList.RemoveAll(t => t.UserName == message.UserName && t.MessageStatus == MessageStatus.Pending);
+                        break;
                 }
-                case MessageStatus.DeletedByUser:
-                    return;
-                case MessageStatus.None:
-                    MessageList.Add(message);
-                    break;
-                case MessageStatus.Pending:
-                    MessageList.Add(message);
-                    break;
-                case MessageStatus.StopPending:
-                    MessageList.RemoveAll(t => t.UserName == message.UserName && t.MessageStatus == MessageStatus.Pending);
-                    break;
-                default:
-                    MessageList.Add(message);
-                    break;
             }
+            else
+            {
+                if(message.MessageStatus == MessageStatus.None)
+                    MessageList.Add(message);
+            }
+            
 
             await eventService.OnMessage(this, EventArgs.Empty);
         });
